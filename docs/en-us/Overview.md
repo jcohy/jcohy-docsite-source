7e46a01fa27a5f8c09ed1e6a452b15ace96aa6fc
jiachao <jia_chao23@126.com>
2019-10-24 18:39:07 +0800

    first commit

---
+++ docs/en-us/Overview.md
@@ -0,0 +1,17 @@
+---
+title: Overview
+keywords: docs，jcohy-docs
+description: Overview
+---
+
+## Document Statement
+
+The series of documents on this website are compiled by the team and some are collected online.
+
+Copyright belongs to the original author. If there is any infringement, please contact us immediately, we will deal with it in time.
+
+## Contact
+
+* Email:jia_chao23@126.com
+
+* QQ:2978685673
--- site_config/docs.js
+++ site_config/docs.js
@@ -1,102 +1,181 @@
 export default {
-  'en-us': {
-    sidemenu: [
-      {
-        title: 'quick-start',
-        children: [
-          {
-            title: 'quick-start',
-            link: '/en-us/docs/quick-start.html',
-          }
+    'en-us': {
+        sidemenu: [
+            {
+                title: 'Overview',
+                children: [
+                    {
+                        title: 'Overview',
+                        link: '/en-us/docs/Overview.html',
+                    },
+                ],
+            },
+            {
+                title: 'Spring Framework',
+                children: [
+                    {
+                        title: 'Spring Framework',
+                        link: '/en-us/docs/springframework/demo.html',
+                    },
+                ],
+            },
+            {
+                title: 'SpringBoot',
+                children: [
+                    {
+                        title: 'SpringBoot',
+                        link: '/en-us/docs/springboot/demo.html',
+                    },
+                ],
+            },
+            {
+                title: 'nginx',
+                children: [
+                    {
+                        title: 'nginx',
+                        link: '/en-us/docs/nginx/demo.html',
+                    },
+                ],
+            },
+            {
+                title: 'Microservices',
+                children: [
+                    {
+                        title: 'Microservices',
+                        link: '/en-us/docs/microservices/demo.html',
+                    },
+                ],
+            },
         ],
-      },
-      {
-        title: 'Spring Framework',
-        children: [
-          {
-            title: 'Spring Framework',
-            link: '/en-us/docs/springframework/demo.html',
-          }
+        barText: 'Documentation',
+    },
+    'zh-cn': {
+        sidemenu: [
+            {
+                title: '概述',
+                children: [
+                    {
+                        title: '概述',
+                        link: '/zh-cn/docs/Overview.html',
+                    },
+                ],
+            },
+            {
+                title: 'Spring Framework',
+                children: [
+                    {
+                        title: '说明',
+                        link: '/zh-cn/docs/springframework/README.html',
+                    },
+                    {
+                        title: '摘要',
+                        link: '/zh-cn/docs/springframework/SUMMARY.html',
+                    },
+                    {
+                        title: '概述',
+                        link: '/zh-cn/docs/springframework/overview/overview.html',
+                    },
+                    {
+                        title: '核心组件',
+                        children: [
+                            {
+                                title: '全部章节一览',
+                                link: '/zh-cn/docs/springframework/core/overview.html',
+                            },
+                            {
+                                title: 'IOC 容器',
+                                link: '/zh-cn/docs/springframework/core/IoC-container.html',
+                            },
+                            {
+                                title: '资源',
+                                link: '/zh-cn/docs/springframework/core/resources.html',
+                            },
+                            {
+                                title: '验证, 数据绑定, 和类型转换',
+                                link: '/zh-cn/docs/springframework/core/validator.html',
+                            },
+                            {
+                                title: 'Spring 表达式语言 (SpEL)',
+                                link: '/zh-cn/docs/springframework/core/SpEL.html',
+                            },
+                            {
+                                title: '使用Spring中面向切面编程',
+                                link: '/zh-cn/docs/springframework/core/aop.html',
+                            },
+                            {
+                                title: 'Spring AOP APIs',
+                                link: '/zh-cn/docs/springframework/core/aop-api.html',
+                            },
+                            {
+                                title: 'null 安全',
+                                link: '/zh-cn/docs/springframework/core/null-safety.html',
+                            },
+                            {
+                                title: '数据缓冲区和编解码器',
+                                link: '/zh-cn/docs/springframework/core/databuffers.html',
+                            },
+                            {
+                                title: 'Appendix',
+                                link: '/zh-cn/docs/springframework/core/appendix.html',
+                            },
+                            ],
+                    },
+                    {
+                        title: '测试',
+                        children: [
+                            {},
+                            {},
+                            {},
+                            {},
+                            ],
+                    },
+                    {
+                        title: 'Web Servlet',
+                        children: [{}],
+                    },
+                    {
+                        title: 'Web Reactive框架',
+                        children: [{}],
+                    },
+                    {
+                        title: '集成',
+                        children: [{}],
+                    },
+                    {
+                        title: '语言',
+                        children: [{}],
+                    },
+                ],
+            },
+            {
+                title: 'SpringBoot',
+                children: [
+                    {
+                        title: 'SpringBoot',
+                        link: '/zh-cn/docs/springboot/demo.html',
+                    },
+                ],
+            },
+            {
+                title: 'nginx',
+                children: [
+                    {
+                        title: 'nginx',
+                        link: '/zh-cn/docs/nginx/demo.html',
+                    },
+                ],
+            },
+            {
+                title: 'Microservices',
+                children:
+                    [
+                        {
+                            title: 'Microservices',
+                            link: '/zh-cn/docs/microservices/demo.html',
+                        },
+                    ],
+            },
         ],
-      },
-      {
-        title: 'SpringBoot',
-        children: [
-          {
-            title: 'SpringBoot',
-            link: '/en-us/docs/springboot/demo.html',
-          },
-        ],
-      },
-      {
-        title: 'nginx',
-        children: [
-          {
-            title: 'nginx',
-            link: '/en-us/docs/nginx/demo.html',
-          },
-        ]
-      },
-      {
-        title: 'Microservices',
-        children: [
-          {
-            title: 'Microservices',
-            link: '/en-us/docs/microservices/demo.html',
-          },
-        ]
-      }
-    ],
-    barText: 'Documentation',
-  },
-  'zh-cn': {
-    sidemenu: [
-      {
-        title: 'quick-start',
-        children: [
-          {
-            title: 'quick-start',
-            link: '/en-us/docs/quick-start.html',
-          }
-        ],
-      },
-      {
-        title: 'Spring Framework',
-        children: [
-          {
-            title: 'Spring Framework',
-            link: '/zh-cn/docs/springframework/demo.html',
-          }
-        ],
-      },
-      {
-        title: 'SpringBoot',
-        children: [
-          {
-            title: 'SpringBoot',
-            link: '/zh-cn/docs/springboot/demo.html',
-          },
-        ],
-      },
-      {
-        title: 'nginx',
-        children: [
-          {
-            title: 'nginx',
-            link: '/zh-cn/docs/nginx/demo.html',
-          },
-        ]
-      },
-      {
-        title: 'Microservices',
-        children: [
-          {
-            title: 'Microservices',
-            link: '/zh-cn/docs/microservices/demo.html',
-          },
-        ]
-      }
-    ],
-    barText: '文档',
-  }
+        barText: '文档',
+    },
 };
